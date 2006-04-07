ac957baa979f9c8562af929559bfb9c07691abbc
Neil Brown <neilb@suse.de>
2006-04-07 04:34:45 +0000

    Fix alignment problem in version-1 superblocks.
    NOTE: This is an incompatable change affecting raid5 reshape.
    If you want to reshape a raid5 using version-1 superblocks,
    use 2.6.17-rc2 or later, and mdadm-2.4.1 or later.
    
    Signed-off-by: Neil Brown <neilb@suse.de>

--- super1.c
+++ super1.c
@@ -58,8 +58,8 @@ struct mdp_superblock_1 {
 				 */
 
 	/* These are only valid with feature bit '4' */
-	__u64	reshape_position;	/* next address in array-space for reshape */
 	__u32	new_level;	/* new level we are reshaping to		*/
+	__u64	reshape_position;	/* next address in array-space for reshape */
 	__u32	delta_disks;	/* change in number of raid_disks		*/
 	__u32	new_layout;	/* new layout					*/
 	__u32	new_chunk;	/* new chunk size (bytes)			*/

172356c93a787fe9cddfa5bbc6008b9ded6dd6f0
Dan Williams <dan.j.williams@intel.com>
2010-08-09 10:26:24 -0700

    Incremental: accept '--no-degraded' as a deprecated option
    Commit 3288b419 (Revert "Incremental: honor --no-degraded to delay assembly")
    killed the --no-degraded flag since commit 97b4d0e9 (Incremental: honor
    an 'enough' flag from external handlers) made this the default behavior
    of -I, and brought -I usage for external/container formats in line with
    native metadata.  However, this breaks existing usages of '-I
    --no-degraded', so allow it as a deprecated option.
    
    Starting a degraded container, like the native metadata case, requires -R.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Reported-by: Ignacy Kasperowicz <ignacy.kasperowicz@intel.com>

--- mdadm.c
+++ mdadm.c
@@ -691,6 +691,8 @@ int main(int argc, char *argv[])
 		"     'summaries', 'homehost', 'byteorder', 'devicesize'.\n");
 			exit(outf == stdout ? 0 : 2);
 
+		case O(INCREMENTAL,NoDegraded):
+			fprintf(stderr, Name ": --no-degraded is deprecated in Incremental mode\n");
 		case O(ASSEMBLE,NoDegraded): /* --no-degraded */
 			runstop = -1; /* --stop isn't allowed for --assemble,
 				       * so we overload slightly */

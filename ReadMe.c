866f509fb934a62ab2bc736214ee14636b8fb051
Albert Pauw <albert.pauw@gmail.com>
2011-02-27 15:56:54 +1100

    FIX: ReadMe.c -Y option missing in short_options
    Hi Neil,
    
    I noticed that the -Y option, as in mdadm -D -Y /dev/md0, doesn't work
    but used as --export it works.
    
    So I made a little patch to fix it, but it is simply sticking a Y in the
    list of short_options in ReadMe.c.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- ReadMe.c
+++ ReadMe.c
@@ -86,11 +86,11 @@ char Version[] = Name " - v3.1.4 - 31st August 2010\n";
  *     At the time if writing, there is only minimal support.
  */
 
-char short_options[]="-ABCDEFGIQhVXWZ:vqbc:i:l:p:m:n:x:u:c:d:z:U:N:sarfRSow1tye:";
+char short_options[]="-ABCDEFGIQhVXYWZ:vqbc:i:l:p:m:n:x:u:c:d:z:U:N:sarfRSow1tye:";
 char short_bitmap_options[]=
-                   "-ABCDEFGIQhVXWZ:vqb:c:i:l:p:m:n:x:u:c:d:z:U:N:sarfRSow1tye:";
+                   "-ABCDEFGIQhVXYWZ:vqb:c:i:l:p:m:n:x:u:c:d:z:U:N:sarfRSow1tye:";
 char short_bitmap_auto_options[]=
-                   "-ABCDEFGIQhVXWZ:vqb:c:i:l:p:m:n:x:u:c:d:z:U:N:sa:rfRSow1tye:";
+                   "-ABCDEFGIQhVXYWZ:vqb:c:i:l:p:m:n:x:u:c:d:z:U:N:sa:rfRSow1tye:";
 
 struct option long_options[] = {
     {"manage",    0, 0, '@'},

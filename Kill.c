88657e86d9c75d3ab68a03035fbc49366712558c
NeilBrown <neilb@suse.de>
2011-11-01 13:30:44 +1100

    Kill: remove duplicate tests on 'force'.
    We test 'force' twice with the second having not chance of
    taking effect.
    As a result a subsequent message - intended for use in the 'force'
    case is never generated.
    
    So remove the first test - it is unnecessary.
    
    Reported-by: Jes.Sorensen@redhat.com
    Signed-off-by: NeilBrown <neilb@suse.de>

--- Kill.c
+++ Kill.c
@@ -61,9 +61,7 @@ int Kill(char *dev, struct supertype *st, int force, int quiet, int noexcl)
 	}
 	st->ignore_hw_compat = 1;
 	rv = st->ss->load_super(st, fd, dev);
-	if (force && rv >= 2)
-		rv = 0; /* ignore bad data in superblock */
-	if (rv== 0 || (force && rv >= 2)) {
+	if (rv == 0 || (force && rv >= 2)) {
 		st->ss->free_super(st);
 		st->ss->init_super(st, NULL, 0, "", NULL, NULL);
 		if (st->ss->store_super(st, fd)) {

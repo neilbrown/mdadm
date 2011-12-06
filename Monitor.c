d97a5e60506343bd8cb2b75d5003b2a49cbc8566
Sergey B Kirpichev <skirpichev@gmail.com>
2011-12-07 08:41:57 +1100

    Report raid level type to syslog on RebuildFinished event.
    Thus, for RAID1/RAID10 this can be filtered out in logcheck.
    
    Relates-to: Debian bug 599821
    Signed-off-by: NeilBrown <neilb@suse.de>

--- Monitor.c
+++ Monitor.c
@@ -564,7 +564,8 @@ static int check_array(struct state *st, struct mdstat_ent *mdstat,
 			sysfs_read(-1, st->devnum, GET_MISMATCH);
 		if (sra && sra->mismatch_cnt > 0) {
 			char cnt[40];
-			sprintf(cnt, " mismatches found: %d", sra->mismatch_cnt);
+			sprintf(cnt, " mismatches found: %d (on raid level %d)",
+				sra->mismatch_cnt, array.level);
 			alert("RebuildFinished", dev, cnt, ainfo);
 		} else
 			alert("RebuildFinished", dev, NULL, ainfo);

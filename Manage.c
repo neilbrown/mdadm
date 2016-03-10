neilbrown/mdadm

commit d180d2aa2a1770af1ab8520d6362ba331400512f

Manage: fix test for 'is array failed'.
We 'active_disks' does not count spares, so if array is rebuilding,
this will not necessarily find all devices, so may report an array
as failed when it isn't.

Counting up to nr_disks is better.

Signed-off-by: NeilBrown <neilb@suse.de>

Manage.c
@@ -827,7 +827,7 @@ int Manage_add(int fd, int tfd, struct mddev_dev *dv,
 			int d;
 			int found = 0;
 
-			for (d = 0; d < MAX_DISKS && found < array->active_disks; d++) {
+			for (d = 0; d < MAX_DISKS && found < array->nr_disks; d++) {
 				disc.number = d;
 				if (ioctl(fd, GET_DISK_INFO, &disc))
 					continue;

commit d31d0f5218e96a2a3a0c9b4418bb387ec211dd0d

Fix regression during add devices
Commit d180d2aa2a17 ("Manage: fix test for 'is array failed'.")
introduced a regression which would not allow to re-add new
drivers to a failed array.

Fixes: d180d2aa2a17 ("Manage: fix test for 'is array failed'.")
Signed-off-by: Hannes Reinecke <hare@suse.de>
Cc: Coly Li <colyli@suse.de>
Cc: Neil Brown <neilb@suse.com>
Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>

Manage.c
@@ -880,10 +880,10 @@ int Manage_add(int fd, int tfd, struct mddev_dev *dv,
 					continue;
 				if (disc.major == 0 && disc.minor == 0)
 					continue;
-				found++;
 				if (!(disc.state & (1<<MD_DISK_SYNC)))
 					continue;
 				avail[disc.raid_disk] = 1;
+				found++;
 			}
 			array_failed = !enough(array->level, array->raid_disks,
 					       array->layout, 1, avail);
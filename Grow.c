56e1e6ace095893b361f26e750eee58fe3068340
Zhilong Liu <zlliu@suse.com>
2017-11-23 11:10:44 +0800

    mdadm/grow: correct the s->size > 1 to make 'max' work
    s->size > 1 : s->size is '1' when '--grow --size max'
    parameter is specified, so correct this test here.
    
    Fixes: 1b21c449e6f2 ("mdadm/grow: adding a test to ensure resize was required")
    Signed-off-by: Zhilong Liu <zlliu@suse.com>
    Signed-off-by: Jes Sorensen <jsorensen@fb.com>

--- Grow.c
+++ Grow.c
@@ -1815,7 +1815,7 @@ int Grow_reshape(char *devname, int fd,
 		return 1;
 	}
 
-	if (array.level > 1 && s->size > 0 &&
+	if (array.level > 1 && s->size > 1 &&
 	    (array.chunk_size / 1024) > (int)s->size) {
 		pr_err("component size must be larger than chunk size.\n");
 		return 1;

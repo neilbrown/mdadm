neilbrown/mdadm

commit 5cb859962febacba3bb9257fc6ed9553ecc16752

Query: Remove all references to md_get_version()
More legacy code removed

Signed-off-by: Jes Sorensen <Jes.Sorensen@gmail.com>

Query.c
@@ -33,7 +33,6 @@ int Query(char *dev)
 	 * a superblock
 	 */
 	int fd = open(dev, O_RDONLY);
-	int vers;
 	int ioctlerr;
 	int superror;
 	struct mdinfo info;
@@ -52,7 +51,6 @@ int Query(char *dev)
 		return 1;
 	}
 
-	vers = md_get_version(fd);
 	if (md_get_array_info(fd, &array) < 0)
 		ioctlerr = errno;
 	else
@@ -60,16 +58,12 @@ int Query(char *dev)
 
 	fstat(fd, &stb);
 
-	if (vers>=9000 && !ioctlerr) {
+	if (!ioctlerr) {
 		if (!get_dev_size(fd, NULL, &larray_size))
 			larray_size = 0;
 	}
 
-	if (vers < 0)
-		printf("%s: is not an md array\n", dev);
-	else if (vers < 9000)
-		printf("%s: is an md device, but kernel cannot provide details\n", dev);
-	else if (ioctlerr == ENODEV)
+	if (ioctlerr == ENODEV)
 		printf("%s: is an md device which is not active\n", dev);
 	else if (ioctlerr)
 		printf("%s: is an md device, but gives \"%s\" when queried\n",
@@ -100,8 +94,7 @@ int Query(char *dev)
 			disc.number = info.disk.number;
 			activity = "undetected";
 			if (mddev && (fd = open(mddev, O_RDONLY))>=0) {
-				if (md_get_version(fd) >= 9000 &&
-				    md_get_array_info(fd, &array) >= 0) {
+				if (md_get_array_info(fd, &array) >= 0) {
 					if (md_get_disk_info(fd, &disc) >= 0 &&
 					    makedev((unsigned)disc.major,(unsigned)disc.minor) == stb.st_rdev)
 						activity = "active";

commit 27e39ad31cbdfe516f9f390cc860a4f681750ef0

Check major number of block device when querying md device
It give error message when query a non md device.
mdadm /dev/null
/dev/null: is an md device, but gives "Inappropriate ioctl for device" when queried

It's introduced by commit 5cb8599 and 8d0cd09
At first it checks whether a block is md device by function md_get_version.
In this function it does mainly two jobs:
1. send request by ioctl. (now it can be replace by argument ioctlerr)
2. check the block device major number which we don't do this.

We add the second judgement in this patch.

Fixes: 5cb8599 and 8d0cd09
Reported-by: Karsten Weiss <karsten.weiss@atos.net>
Signed-off-by: Xiao Ni <xni@redhat.com>
Signed-off-by: Jes Sorensen <jsorensen@fb.com>

Query.c
@@ -85,11 +85,10 @@ int Query(char *dev)
 
 	if (ioctlerr == ENODEV)
 		printf("%s: is an md device which is not active\n", dev);
+	else if (ioctlerr && major(stb.st_rdev) != MD_MAJOR)
+		printf("%s: is not an md array\n", dev);
 	else if (ioctlerr)
 		printf("%s: is an md device, but gives \"%s\" when queried\n",
-		       dev, strerror(ioctlerr));
-	else if (staterr)
-		printf("%s: is not a valid md device, returning %s\n",
 		       dev, strerror(ioctlerr));
 	else {
 		printf("%s: %s %s %d devices, %d spare%s. Use mdadm --detail for more detail.\n",